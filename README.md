5488e19d2a3c732c8bbf027ece6bcc78ebf54b8c
Dieter Reuter <dieter.reuter@me.com>
2017-04-03 23:39:33 +0200

    Move remark.min.js into assets folder
    Signed-off-by: Dieter Reuter <dieter.reuter@me.com>

--- README.md
+++ README.md
@@ -16,12 +16,13 @@ $ ./update-deps.sh
 
 ### List slide files
 ```
-$ ls -alR ./slides/
-total 1312
-drwxr-xr-x  4 dieter  staff     136 Apr  3 23:01 .
-drwxr-xr-x  9 dieter  staff     306 Apr  3 23:01 ..
--rw-r--r--  1 dieter  staff    1150 Apr  3 23:01 index.html
--rw-r--r--  1 dieter  staff  664929 Apr  3 23:01 remark.min.js
+$ ls -alR ./slides
+drwxr-xr-x  5 dieter  staff   170 Apr  3 23:35 assets
+-rw-r--r--  1 dieter  staff  4961 Apr  3 23:33 index.html
+
+
+./slides/assets:
+-rw-r--r--  1 dieter  staff  664929 Apr  3 23:35 remark.min.js
 ```
 
 ### Start presentation
--- update-deps.sh
+++ update-deps.sh
@@ -4,16 +4,16 @@ set -x
 
 # Download assets
 REMARK_VERSION=0.14.0
-mkdir -p slides
+mkdir -p slides/assets
 
 #- remark.min.js
 curl -sSLO https://gnab.github.io/remark/downloads/remark-${REMARK_VERSION}.min.js
-cp remark-${REMARK_VERSION}.min.js slides/remark.min.js
+cp remark-${REMARK_VERSION}.min.js slides/assets/remark.min.js
 
 #- index.html
 curl -sSLO https://github.com/gnab/remark/raw/v${REMARK_VERSION}/boilerplate-local.html
 if [[ ! -f slides/index.html ]]; then
   cp boilerplate-local.html slides/index.html
-  sed -i.bak 's/out\/remark.js/remark.min.js/g' slides/index.html
+  sed -i.bak 's/out\/remark.js/assets\/remark.min.js/g' slides/index.html
   rm -f slides/index.html.bak
 fi
